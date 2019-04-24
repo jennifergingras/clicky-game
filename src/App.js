3b3a3cfbdbf0fb0bb7a0d9bbc94d917d5cdeb9e3
Jennifer Gingras <jgingras@meca.edu>
2019-04-24 12:12:56 -0400

    setting up files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header"
+import patternCard from "./components/patternCard";
+import cards from "./cards.json";
 
 class App extends Component {
+  state = {
+    cards
+  };
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        {this.state.cards.map(card => (
+          <patternCard
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))};
       </div>
     );
-  }
+  };
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css";
+
+const Header = (props) => {
+  return (
+    <div>
+      <div className="header-container">
+        <h1>Memory Click</h1>
+        <p>There are 12 patterns below. Test your memory by clicking each of them only once.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
